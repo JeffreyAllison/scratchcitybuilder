eb6e1949b9b835f5e58ae5da4d6ad06de4809ca0
JeffAllison <itsjallison@gmail.com>
2022-03-11 10:02:00 -0800

    added event listeners for city and rural images/dropdowns

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cityNameEl = document.getElementById('city-name');
 const waterDropdown = document.getElementById('water-dropdown');
 const cityDropdown = document.getElementById('city-dropdown');
 const ruralDropdown = document.getElementById('rural-dropdown');
-const countMessageEl = document.getElementById('count-message');
+const counterMessageEl = document.getElementById('count-message');
 const waterImgEl = document.getElementById('water-img');
 const cityImgEl = document.getElementById('city-img');
 const ruralImgEl = document.getElementById('rural-img');
@@ -38,6 +38,18 @@ cityNameInput.addEventListener('input', () => {
 waterDropdown.addEventListener('change', () => {
   waterCount++;
   waterImgEl.src = `assets/water-${waterDropdown.value}.png`;
+  displayCounterText();
+});
+
+cityDropdown.addEventListener('change', () => {
+  cityCount++;
+  cityImgEl.src = `assets/city-${cityDropdown.value}.png`;
+  displayCounterText();
+});
+
+ruralDropdown.addEventListener('change', () => {
+  ruralCount++;
+  ruralImgEl.src = `assets/rural-${ruralDropdown.value}.png`;
   displayCounterText();
 });
 
